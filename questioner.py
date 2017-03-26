ccff21d377c9aef3581ef51a4bb418c41a4c9b91
whizzer0 <whizzerZero@gmail.com>
2017-03-26 12:54:38 +0100

    Modular question generator almost finished

--- questioner.py
+++ questioner.py
@@ -1,33 +1,38 @@
 import random
 from sigfigs import f
 
-form = {"vdt1":"velocity", "vdt2":"distance", "vdt3":"time"}
-unit = {"vdt1":" m/s", "vdt2":"m", "vdt3":" seconds"}
+eqs = ["dvt"]
+form = {"dvt1":"distance", "dvt2":"velocity", "dvt3":"time"}
+unit = {"dvt1":"m", "dvt2":" m/s", "dvt3":" seconds"}
 
 def generateQuestions():
   questions = {}
   i = 5
   while i > 0:
-    value1 = random.randint(1,500)
-    value2 = random.randint(1,500)
-    solvalue = random.randint(1,3)
-    #1: velocity (m/s), 2: distance (m), 3: time (s)
-    if solvalue == 1:
-      if value1 > value2 or random.randint(1,5) == 1:
-        questions["q" + str(i)] = "If distance is " + str(value1) + "m and time is " + str(value2) + " seconds, what is the velocity?"
-        questions["a" + str(i)] = f(float(value1) / float(value2), 3)
-      else:
-        questions["q" + str(i)] = "If distance is " + str(value2) + "m and time is " + str(value1) + " seconds, what is the velocity?"
-        questions["a" + str(i)] = f(float(value2) / float(value1), 3)
-      questions["u" + str(i)] = " m/s"
-    elif solvalue == 2:
-      questions["q" + str(i)] = "If velocity is " + str(value1) + " m/s and time is " + str(value2) + " seconds, what is the distance?"
+    value1 = random.randint(2,500)
+    value2 = random.randint(2,500)
+    eq = eqs[random.randint(0,len(eqs)-1)]
+    order = 1
+    if order == 1:
+      questions["q" + str(i)] = "If the " + form[eq + "2"] + " is " + str(value1) + unit[eq + "2"] + " and the " + form[eq + "3"] + " is " + str(value2) + unit[eq + "3"] + ", what is the " + form[eq + "1"] + "?"
       questions["a" + str(i)] = f(value1 * value2, 3)
-      questions["u" + str(i)] = "m"
-    elif solvalue == 3:
-      questions["q" + str(i)] = "If velocity is " + str(value1) + " m/s and distance is " + str(value2) + "m, what is the time?"
-      questions["a" + str(i)] = f(value1 * value2, 3)
-      questions["u" + str(i)] = " seconds"
+      questions["u" + str(i)] = unit[eq + "1"]
+#   if solvalue == 1:
+#     if value1 > value2 or random.randint(1,5) == 1:
+#       questions["q" + str(i)] = "If distance is " + str(value1) + "m and time is " + str(value2) + " seconds, what is the velocity?"
+#       questions["a" + str(i)] = f(float(value1) / float(value2), 3)
+#     else:
+#       questions["q" + str(i)] = "If distance is " + str(value2) + "m and time is " + str(value1) + " seconds, what is the velocity?"
+#       questions["a" + str(i)] = f(float(value2) / float(value1), 3)
+#     questions["u" + str(i)] = " m/s"
+#   elif solvalue == 2:
+#     questions["q" + str(i)] = "If velocity is " + str(value1) + " m/s and time is " + str(value2) + " seconds, what is the distance?"
+#     questions["a" + str(i)] = f(value1 * value2, 3)
+#     questions["u" + str(i)] = "m"
+#   elif solvalue == 3:
+#     questions["q" + str(i)] = "If velocity is " + str(value1) + " m/s and distance is " + str(value2) + "m, what is the time?"
+#     questions["a" + str(i)] = f(value1 * value2, 3)
+#     questions["u" + str(i)] = " seconds"
     i = i - 1
   #print(questions)
   return questions
