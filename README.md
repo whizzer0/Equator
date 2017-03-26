945b1958d71850d5a6bf755aeee8579add584290
whizzer0 <whizzerZero@gmail.com>
2017-03-26 11:44:23 +0100

    Now it actually marks your work!

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Equator
 
-EQUATOR is an exciting app that will enhance your learning of equations. Try EQUATOR today!
+Equator is a tool to generate questions to test your knowledge of GCSE Physics formulae. It is part of the Material Revision project.
+
+Equator uses:
+
+* [Material Design Lite](https://github.com/google/material-design-lite)
+* [Material Design Icons](https://github.com/Templarian/MaterialDesign)
+
+## Progress
+
+Equator is very much a WIP and isn't really usable yet. Currently, Equator can:
+
+* Generate and offer questions involving the formula velocity = distance ÷ time
+* Accept user input for these questions and mark them correct/incorrect
+* Look fancy (drop shadows make everything better)
+
+## Soon™
+
+Currently working on making Equator able to:
+
+* Show the correct answer to the questions when you get them wrong
+* Use multiple equations
+* Allow the user to select an area of the syllabus from which to test equations
--- questioner.py
+++ questioner.py
@@ -16,7 +16,7 @@ def generateQuestions():
       else:
         questions["q" + str(i)] = "If distance is " + str(value2) + "m and time is " + str(value1) + " seconds, what is the velocity?"
         questions["a" + str(i)] = f(float(value2) / float(value1), 3)
-      questions["u" + str(i)] = "m/s"
+      questions["u" + str(i)] = " m/s"
     elif solvalue == 2:
       questions["q" + str(i)] = "If velocity is " + str(value1) + " m/s and time is " + str(value2) + " seconds, what is the distance?"
       questions["a" + str(i)] = f(value1 * value2, 3)
@@ -24,7 +24,7 @@ def generateQuestions():
     elif solvalue == 3:
       questions["q" + str(i)] = "If velocity is " + str(value1) + " m/s and distance is " + str(value2) + "m, what is the time?"
       questions["a" + str(i)] = f(value1 * value2, 3)
-      questions["u" + str(i)] = "seconds"
+      questions["u" + str(i)] = " seconds"
     i = i - 1
   #print(questions)
   return questions
@@ -33,23 +33,43 @@ def processResults(questions, q1, q2, q3, q4, q5):
   corrects = {}
   if f(q1,3) == questions["a1"]:
     corrects["q1"] = True
+    corrects["s1"] = "✓"
+    corrects["c1"] = "#4CAF50"
   else:
     corrects["q1"] = False
+    corrects["s1"] = "✗"
+    corrects["c1"] = "#F44336"
   if f(q1,3) == questions["a2"]:
     corrects["q2"] = True
+    corrects["s2"] = "✓"
+    corrects["c2"] = "#4CAF50"
   else:
     corrects["q2"] = False
+    corrects["s2"] = "✗"
+    corrects["c2"] = "#F44336"
   if f(q1,3) == questions["a3"]:
     corrects["q3"] = True
+    corrects["s3"] = "✓"
+    corrects["c3"] = "#4CAF50"
   else:
     corrects["q3"] = False
+    corrects["s3"] = "✗"
+    corrects["c3"] = "#F44336"
   if f(q1,3) == questions["a4"]:
     corrects["q4"] = True
+    corrects["s4"] = "✓"
+    corrects["c4"] = "#4CAF50"
   else:
     corrects["q4"] = False
+    corrects["s4"] = "✗"
+    corrects["c4"] = "#F44336"
   if f(q1,3) == questions["a5"]:
     corrects["q5"] = True
+    corrects["s5"] = "✓"
+    corrects["c5"] = "#4CAF50"
   else:
     corrects["q5"] = False
+    corrects["s5"] = "✗"
+    corrects["c5"] = "#F44336"
   print(corrects)
   return(corrects)
